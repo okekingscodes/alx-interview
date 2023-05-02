8d94a4ccb6815f0794ac9e767d4899998a8f2595
Odim Kingsley <okekings@gmail.com>
2023-05-02 22:25:47 +0000

    Hot fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""Pascal Triangle Interview Challenge for Alx project"""
+"""Pascal"""
 
 
 def pascal_triangle(n):
+    """fucnrion to get the number of pascal triangle"""
     if n <= 0:
         return []
 
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
         for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
 
-            row.append(triangle[-1][j-1] + triangle[-1][j])
-        row.append(1)
-    triangle.append(row)
+        pascal_triangle[i] = new_row
 
-    return triangle
+    return pascal_triangle
